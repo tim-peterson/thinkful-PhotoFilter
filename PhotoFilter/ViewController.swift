ac4580ad5ec96a38d2ae66ecc41fb190444e6a87
tim-peterson <petersontimr@gmail.com>
2014-10-31 12:32:35 -0400

    1st commit, photo still offset

--- PhotoFilter/ViewController.swift
+++ PhotoFilter/ViewController.swift
@@ -10,6 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var photoImageView: UIImageView!
+
+    let context = CIContext(options: nil)
+ 
+    
+    @IBAction func applyFilter(sender: AnyObject) {
+        
+        let inputImage = CIImage(image: photoImageView.image)
+        
+        let randomColor = [kCIInputAngleKey: (Double(arc4random_uniform(314)) / 100)]
+        
+        let filteredImage = inputImage.imageByApplyingFilter("CIHueAdjust", withInputParameters: randomColor)
+        
+        let renderedImage = context.createCGImage(filteredImage, fromRect: filteredImage.extent())
+        
+        photoImageView.image = UIImage(CGImage: renderedImage)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
